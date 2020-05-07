3a808dc72875c6073eda6665d5bb1ad28d1c5f3a
noahrslavik <noahrobertslavik@gmail.com>
2020-05-07 11:23:58 -0700

    Fixed

--- src/MIdterm.java
+++ src/MIdterm.java
@@ -2,11 +2,8 @@
 public class MIdterm {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named David and his trusty band of ninja students");
-=======
-		System.out.println("Once upon a time in the mystical land of Detroit there dwelt an immortal named Kai-Yun and his trusty band of ninja monkey");
->>>>>>> d66e2948f98bf300afdea0bbcf8a368c6e07223c
+
+		System.out.println("Once upon a time in the mystical land of Detroit there dwelt an immortal named David and his trusty band of ninja students!");
 		System.out.println("They were very clever monkeys indeed, known for their ingenious teamwork.");
 		System.out.println("They rarely slept, thriving off monkey booze and bananas, bullying the travellers of the land");
 	    System.out.println("We went to the circus");
